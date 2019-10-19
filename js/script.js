2616821cf975810aff110fc8b8624eb79722ef3f
Paulo Roberto <paulordev@gmail.com>
2019-10-19 12:50:00 -0300

    Teste docket

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('.carrousel').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true
+});
+
+
+// anime js
+if (window.SimpleAnime) {
+    new SimpleAnime();
+
+}
